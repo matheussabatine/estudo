407d7ea93ddf873da84fe8881ed5fa9f6b515f7a
matheussabatine <70068237+matheussabatine@users.noreply.github.com>
2024-04-12 15:24:10 -0300

    Add files via upload

--- c#/Program.cs
+++ c#/Program.cs
@@ -17,31 +17,52 @@
 // double
 // decimal
 // string
+class Program{
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("Parabéns!");
-
-bool acesso = false;
-
-// operador ternário: expressao ? valor1 : valor2;
-// se expressao for verdade, é realizado valor1
-// se expressao for falsa, é realizado valor2
-int i;
-i = 5 > 10 ? 5 : 15;
-
-Console.WriteLine(i);
-
-//programa simples
-Console.WriteLine("digite o nome...");
-// Nullable Types: possiveis nulos, usa ?
-string? nome;
-nome = Console.ReadLine();
-Console.WriteLine("seu nome é " + nome);
-Console.WriteLine($"seu nome é {nome}");
-
-Console.WriteLine("digite um numero...");
-int? numero;
-numero = int.Parse(Console.ReadLine());
-Console.WriteLine($"seu numero é {numero}");
+    static void Main(){
 
 
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Parabéns!");
+
+        /*
+        bool acesso = false;
+
+        // operador ternário: expressao ? valor1 : valor2;
+        // se expressao for verdade, é realizado valor1
+        // se expressao for falsa, é realizado valor2
+        int i;
+        i = 5 > 10 ? 5 : 15;
+
+        Console.WriteLine(i);
+
+        //programa simples
+        Console.WriteLine("digite o nome...");
+        // Nullable Types: possiveis nulos, usa ?
+        string? nome;
+        nome = Console.ReadLine();
+        Console.WriteLine("seu nome é " + nome);
+        Console.WriteLine($"seu nome é {nome}");
+
+        Console.WriteLine("digite um numero...");
+        int? numero;
+        numero = int.Parse(Console.ReadLine());
+        Console.WriteLine($"seu numero é {numero}");
+        */
+        string[] lista = new string[4];
+        Console.WriteLine(lista.Length);
+
+        //coversao explicita
+        long i = 1;
+        int j = 3;
+
+        //para conversões para baixo, de long(64bits) para int(32bits), deve-se explicitamente converte-lo.
+        
+        j = (int)i;
+
+        //para conversões para cima, conversões implicitas
+        i = j;
+
+
+    }
+}
